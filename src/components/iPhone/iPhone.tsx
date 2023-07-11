33f438a4cee0da8c4c2e5d0a5c37df0d2e1e7520
Oleksandr Kazimirchuk <lekskazimirchuk@gmail.com>
2023-07-11 19:58:12 +0300

    Add skeleton

---
+++ src/components/iPhone/iPhone.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface iPhoneProps {
+}
+
+const iPhone: React.FC<iPhoneProps> = (props) => {
+    return (
+        <div>
+        <h1>iPhone</h1>
+        </div>
+    )
+}
+
+export default iPhone;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as iPhone } from "./iPhone";
