d3648a617fa499da88bc91216c049c96641b4803
Oleksandr Kazimirchuk <lekskazimirchuk@gmail.com>
2023-07-11 20:20:32 +0300

    Add dummy styles

--- src/components/iPhone/iPhone.tsx
+++ src/components/iPhone/iPhone.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 
 interface iPhoneProps {
+    colorMode: "light" | "dark";
 }
 
-const iPhone: React.FC<iPhoneProps> = (props) => {
+const IPHONE_HEIGHT = 5.81; // inches
+const IPHONE_WIDTH = 2.81; // inches
+
+const iPhone: React.FC<iPhoneProps> = ({
+    colorMode
+}: iPhoneProps) => {
     return (
-        <div>
-        <h1>iPhone</h1>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: `${IPHONE_HEIGHT}rem`,
+                width: `${IPHONE_WIDTH}rem`,
+                backgroundColor: colorMode === "light" ? "#ebebeb" : "white",
+            }}
+        >
+            <h1>iPhone</h1>
         </div>
     )
 }
