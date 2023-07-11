b57e5d7b6558831a8838f8c88d24f7dc9b136c92
Oleksandr Kazimirchuk <lekskazimirchuk@gmail.com>
2023-07-11 20:40:22 +0300

    Optimize build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
@@ -21,9 +23,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
